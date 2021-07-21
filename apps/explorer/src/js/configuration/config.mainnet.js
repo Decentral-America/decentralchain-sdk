4ef6432081bd72b7d58dc72ea8518b02a44ab29a
Gabo1122 <gabo_carranza@hotmail.com>
2021-07-20 23:13:26 -0600

    Update connection to mainnet

--- apps/explorer/src/js/configuration/config.mainnet.js
+++ apps/explorer/src/js/configuration/config.mainnet.js
@@ -1,4 +1,4 @@
-const nodeUrl = 'https://nodes.wavesexplorer.com';
+const nodeUrl = 'https://mainnet-node.decentralchain.io';
 
 export default {
     networkId: 'mainnet',
@@ -7,5 +7,5 @@ export default {
     useCustomRequestConfig: true,
     dataServicesBaseUrl: 'https://api.wavesplatform.com/v0',
     spamListUrl: 'https://raw.githubusercontent.com/wavesplatform/waves-community/master/Scam%20tokens%20according%20to%20the%20opinion%20of%20Waves%20Community.csv',
-    nodes: [{url: nodeUrl, maintainer: 'Waves'}]
+    nodes: [{url: nodeUrl, maintainer: 'DCC'}]
 };
