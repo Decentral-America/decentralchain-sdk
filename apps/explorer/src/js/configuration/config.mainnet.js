40f15a1b26cedfa04003a4b551f106e391fd3f36
bnagtega <brn@softwareag.com>
2021-07-24 19:52:03 +0200

    setup mainnet settings + nodes

--- apps/explorer/src/js/configuration/config.mainnet.js
+++ apps/explorer/src/js/configuration/config.mainnet.js
@@ -7,5 +7,16 @@ export default {
     useCustomRequestConfig: true,
     dataServicesBaseUrl: 'https://api.wavesplatform.com/v0',
     spamListUrl: 'https://raw.githubusercontent.com/wavesplatform/waves-community/master/Scam%20tokens%20according%20to%20the%20opinion%20of%20Waves%20Community.csv',
-    nodes: [{url: nodeUrl, maintainer: 'DCC'}]
+    nodes: [{url: nodeUrl, maintainer: 'DCC',showAsLink: true}
+    , {
+        url: "https://mainnet-node1.decentralchain.io",
+        maintainer: "DCC",
+        showAsLink: true,
+    },
+        {
+            url: "https://mainnet-node2.decentralchain.io",
+            maintainer: "DCC",
+            showAsLink: true,
+        }
+    ]
 };
